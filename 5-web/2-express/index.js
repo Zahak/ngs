c8599cfd1d15966cdb25db24097a4bd588f022ba
Johannes Henrysson-Tholse <johannes.henrysson-tholse@prisjakt.nu>
2021-01-26 12:52:11 +0100

    5 - Web part.

--- 5-web/2-express/index.js
+++ 5-web/2-express/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+
+server.get('/test', (req, res) => {
+  res.send('Hello express test!');
+});
+
 server.listen(4242, () => {
   console.log('Express Server is running...');
 });
